31f89ce957518667cd652bfbfe945296dc4f314c
Abhishek Gade <abhishekgade790@gmail.com>
2025-07-02 14:02:41 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <h1 className="text-2xl p-8">hello world!</h1>
+    </>
+  )
+}
+
+export default App
